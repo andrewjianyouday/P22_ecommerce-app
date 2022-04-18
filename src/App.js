88837bf65a7c98ca54591556ecf7f269f4a19219
andrewjianyouday <andrewjianyouday@gmail.com>
2022-04-18 22:16:05 +0200

    create route path for signin page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route} from 'react-router-dom'
 import ShopPage from './pages/shop/shop.component.shop';
 import HomePage from './pages/homepage/homepage.component';
 import Header from './components/header/header.component'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up.component/sign-in-and-sign-up.component';
 
 import './App.css';
 
@@ -20,6 +21,7 @@ function App(){
       <Switch>
         <Route exact path="/" component={HomePage}/>
         <Route path="/shop" component={ShopPage}/>
+        <Route path="/signin" component={SignInAndSignUpPage}/>
       </Switch>
     </div>
   )
